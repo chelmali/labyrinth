5d5ed4eec242ff2ba6b99b1151639731c964c258
chelmali <Chaymaa.EL_MALI@etu.uca.fr>
2021-06-17 00:43:32 +0200

    les heders du code jeu de vie

---
+++ texture.h
@@ -0,0 +1,6 @@
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+void end_sdl(char ok,char const* msg,SDL_Window* window,SDL_Renderer* renderer);
