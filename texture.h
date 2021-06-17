0f82cc5d00489a9e52284397af31bf31924c6f78
chelmali <Chaymaa.EL_MALI@etu.uca.fr>
2021-06-17 14:18:49 +0200

    header de l'animation texture

--- texture.h
+++ texture.h
@@ -3,4 +3,14 @@
 #include <math.h>
 #include <stdio.h>
 #include <string.h>
+#include <signal.h>
+#include <bits/sigaction.h>
 void end_sdl(char ok,char const* msg,SDL_Window* window,SDL_Renderer* renderer);
+void play_texture1(SDL_Texture * texture,SDL_Window *window,SDL_Renderer * renderer);
+SDL_Texture* load_texture_from_image(char  *  file_image_name, SDL_Window *window, SDL_Renderer *renderer );
+void play_with_texture_2(SDL_Texture* my_texture,SDL_Window* window,SDL_Renderer* renderer) ;
+void play_with_texture_3(SDL_Texture* my_texture,SDL_Window* window,SDL_Renderer* renderer) ;
+//void play_with_texture_4(SDL_Texture* my_texture,SDL_Window* window,SDL_Renderer* renderer);
+void play_with_texture_5(SDL_Texture *bg_texture,SDL_Texture *my_texture,SDL_Window *window,SDL_Renderer *renderer);
+void play_with_texture_1_1(SDL_Texture * texture,SDL_Window *window,SDL_Renderer * renderer);
+void play_with_texture_4(SDL_Texture* my_texture,SDL_Texture * texture2,SDL_Window* window,SDL_Renderer* renderer) ; //autre version de play-with 4 afin de garder la fenetre fixe background
