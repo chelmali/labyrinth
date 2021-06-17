2cb7ea711f5779bd719468ad33d39ee0398b7a17
maoumassou <manar.oumassou@uca.fr>
2021-06-17 09:31:35 +0200

    pavé de serpents

---
+++ snake.h
@@ -0,0 +1,12 @@
+#ifndef SNAKE_H
+#define SNAKE_H
+#include <SDL2/SDL.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include"time.h"
+
+void end_sdl(char ok, char const* msg, SDL_Window* window, SDL_Renderer* renderer);
+void draw(SDL_Renderer* renderer, int a, int b, int w, int h);
+
+#endif
