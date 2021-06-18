399b63fdc1e870ca187e2f40a579e11ce3ad24b1
maoumasou <85866272+maoumasou@users.noreply.github.com>
2021-06-18 04:57:27 +0200

    manipulation des textures

---
+++ texture.h
@@ -0,0 +1,18 @@
+#ifndef TEXTURE_H
+#define TEXTURE_H
+#include <SDL2/SDL.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+
+void end_sdl(char ok,char const* msg,SDL_Window* window,SDL_Renderer* renderer);
+SDL_Texture* load_texture_from_image(char  *  file_image_name, SDL_Window *window, SDL_Renderer *renderer );
+void play_with_texture_1(SDL_Texture *my_texture, SDL_Window *window, SDL_Renderer *renderer);
+void play_with_texture_2(SDL_Texture *my_texture, SDL_Window *window, SDL_Renderer *renderer);
+void play_with_texture_3(SDL_Texture *my_texture, SDL_Texture *bg_texture,SDL_Window *window, SDL_Renderer *renderer);
+void play_with_texture_4(SDL_Texture *my_texture,SDL_Texture *bg_texture, SDL_Window *window, SDL_Renderer *renderer);
+void play_with_texture_5(SDL_Texture *bg_texture,SDL_Texture *my_texture, SDL_Window *window, SDL_Renderer *renderer);
+void play_with_texture_1_1(SDL_Texture *my_texture, SDL_Window *window, SDL_Renderer *renderer);
+
+ #endif
